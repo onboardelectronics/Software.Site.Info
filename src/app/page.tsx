b691d820c6a1aac5df64c66d6cc4f3b80f775f96
Leonardo Valle <leo.mvm@hotmail.com>
2024-06-28 12:01:25 -0300

    favicon + footer + urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { FaAppStoreIos, FaGooglePlay, FaWhatsapp } from "react-icons/fa6"
 import { MdDescription } from "react-icons/md"
 
 export default function Home() {
-    return <nav className="h-dvh w-full flex flex-col items-center">
+    return <nav className="h-full w-full flex flex-col items-center">
         <OnBoardLogo className="m-4 max-w-[36rem]"/>
 
         <div className="w-full md:h-full grid grid-cols-2 sm:grid-cols-4 gap-4 p-8 justify-center items-center">
@@ -14,19 +14,19 @@ export default function Home() {
                       className="bg-white border-white text-black col-span-2 md:col-span-1 md:aspect-square hover:bg-black hover:text-white"
                       icon={<MdDescription size={32}/>}/>
 
-            <LinkCard url=""
+            <LinkCard url="https://wa.me/5531996115502?text=Ol%C3%A1!%20Preciso%20de%20ajuda%20com%20a%20tecnologia%20da%20OnBoard"
                       title="Suporte"
                       subtitle="WhatsApp"
                       className="bg-green-600 border-green-600 text-white col-span-2 md:col-span-1 md:aspect-square hover:bg-white hover:text-green-600"
                       icon={<FaWhatsapp size={32}/>}/>
 
-            <LinkCard url=""
+            <LinkCard url="https://apps.apple.com/br/app/instalador/id6502647804"
                       title="iOS"
                       subtitle="App Instalador"
                       className="bg-blue-600 border-blue-600 text-white aspect-square sm:aspect-auto md:aspect-square sm:col-span-2 md:col-span-1 hover:bg-white hover:text-blue-600"
                       icon={<FaAppStoreIos size={32}/>}/>
 
-            <LinkCard url=""
+            <LinkCard url="https://play.google.com/store/apps/details?id=br.ind.onboard.instalador"
                       title="Android"
                       subtitle="App Instalador"
                       className="bg-green-700 border-green-700 text-white aspect-square sm:aspect-auto md:aspect-square sm:col-span-2 md:col-span-1 hover:bg-white hover:text-green-700"
